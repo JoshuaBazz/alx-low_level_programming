6c3a9ea830d6a2a9ac685bc78800acb0ff2dd97c
joshuabazz <joshuabass2002@gmail.com>
2022-10-14 08:49:46 -0700

    Low level commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Serves as entry for the program*
+ * Return - It reeturns to zero if no error is encountered , otherwise a non-zero value is shown*
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
